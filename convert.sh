b88c9db7963494bd34eb2efcea5eca9911cfc846
Davis Ford <ford_davis@bah.com>
2017-12-29 23:29:44 -0500

    added some more

--- convert.sh
+++ convert.sh
@@ -3,17 +3,46 @@
 # Requires inkscape ( brew install caskformula/caskformula/inkscape )
 # Requires optipng
 
-if [ -d "output/" ]; then
-    cd output/
-    for file in *.svg
-    do
-        inkscape -z -f "${file}" -e "${file%svg}png" -b "#fff" -d 1080
-        if [ "$1" = "-c" ]; then # Run with -c flag to compress png's
-            optipng "${file%svg}png" -o1
-        fi
-    done
+BG_COLOR="#FFF"
+SINGLE_FILEPATH=''
+DPI=512
 
-else
+if [[ ! -z "$1" && "$1" != "" && "$1" != "-a" ]]; then
+    echo "Using $1 for SINGLE_FILEPATH"
+    SINGLE_FILEPATH="$1"
+fi
+
+if [[ ! -z "$2" && "$2" != ""  ]]; then
+    echo "Using $2 for BG_COLOR"
+    BG_COLOR="$2"
+fi
+
+run_single()
+{
+    inkscape -z -f "${SINGLE_FILEPATH}" -e "${SINGLE_FILEPATH%svg}png" -b "${BG_COLOR}" -d ${DPI}
+    optipng "${SINGLE_FILEPATH%svg}png" -o1
+}
+
+run_multiple()
+{
+cd output/
+for file in *.svg
+do
+    inkscape -z -f "${file}" -e "${file%svg}png" -b "${BG_COLOR}" -d ${DPI}
+    optipng "${file%svg}png" -o1
+done
+cd ..
+}
+
+if [[ ! -d "./output/" ]]; then
     echo "No output directory, we're done here"
     exit 0
+fi
+
+# Pass -a to apply the bg color change to all
+if [[ ! -z "$1" && "$1" != "-a"  ]]; then
+    echo "Converting $1"
+    run_single
+else
+    run_multiple
 fi
--- examples.sh
+++ examples.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-python svg.py -f ./txt/bible.txt -c grey
-python svg.py -f ./txt/purple_cow.txt -c "#d40adb"
-python svg.py -f ./txt/romance_of_lust.txt -c "#e51919"
-python svg.py -f ./txt/the_republic.txt
-python svg.py -f ./txt/wizard_of_oz.txt -c "#d1ca0e"
-python svg.py -f ./txt/ulysses.txt -c "#c11387"
+#python svg.py -f txt/bible.txt -c grey && sh convert.sh output/bible.svg
+#python svg.py -f txt/purple_cow.txt -c "#d40adb" && sh convert.sh output/purple_cow.svg
+#python svg.py -f txt/romance_of_lust.txt -c "#e51919" && sh convert.sh output/romance_of_lust.svg
+#python svg.py -f txt/the_republic.txt && sh convert.sh output/the_republic.svg
+#python svg.py -f txt/wizard_of_oz.txt -c yellow && sh convert.sh output/wizard_of_oz.svg "#000" && convert output/wizard_of_oz.png -rotate 270 output/wizard_of_oz.png;
+python svg.py -f txt/space_and_time.txt -c white && sh convert.sh output/space_and_time.svg "#000" && convert output/space_and_time.png -rotate 270 output/space_and_time.png;
+#python svg.py -f txt/ulysses.txt -c "#c11387" && sh convert.sh output/ulysses.svg "#DF169E"
+#python svg.py -f txt/frankenstein.txt -c green && sh convert.sh output/frankenstein.svg black
+#python svg.py -f txt/zen.txt -c "#DF169E" && sh convert.sh output/zen.svg "#00D2D9" && convert output/zen.png -rotate 90 output/zen.png;
 
-sh convert.sh -c
+
